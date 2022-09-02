d796cae740fd32d55719a325222e98ad1c8b818f
k-chop <whelmaze@gmail.com>
2022-09-02 17:15:18 +0900

    色の計算に関する部分を別ファイルに分離

---
+++ src/color.ts
@@ -0,0 +1,71 @@
+import p5 from "p5";
+
+type ColorMapper = {
+  size: number;
+  f: (p: p5, n: number, offset?: number) => p5.Color;
+};
+
+export const posterize = (
+  p: p5,
+  value: number,
+  numberOfTones: number,
+  lower: number,
+  upper: number
+) => {
+  const paletteLength = numberOfTones * 2;
+  const v = value % paletteLength;
+
+  if (v < numberOfTones) {
+    return p.map(Math.floor(v % numberOfTones), 0, numberOfTones, lower, upper);
+  } else {
+    return p.map(Math.floor(v % numberOfTones), 0, numberOfTones, upper, lower);
+  }
+};
+
+const colors: ColorMapper[] = [
+  {
+    size: 256,
+    f: (p, n) => {
+      // hue 0~360
+      const hue = posterize(p, n, 128, 0, 360);
+      return p.color(hue, 75, 100);
+    },
+  },
+  {
+    size: 256,
+    f: (p, n) => {
+      // monochrome
+      const brightness = posterize(p, n, 128, 20, 100);
+      return p.color(0, 0, brightness);
+    },
+  },
+  {
+    size: 256,
+    f: (p, n) => {
+      // fire
+      const brightness = posterize(p, n, 128, 30, 100);
+      return p.color(0, 90, brightness);
+    },
+  },
+];
+
+export const buildColors = (p: p5) => {
+  const result: Uint8ClampedArray[] = [];
+
+  colors.forEach((colorMapper) => {
+    const array = new Uint8ClampedArray(colorMapper.size * 4);
+
+    for (let i = 0; i < colorMapper.size; i++) {
+      const color = colorMapper.f(p, i);
+      const idx = i * 4;
+      array[idx + 0] = p.red(color);
+      array[idx + 1] = p.green(color);
+      array[idx + 2] = p.blue(color);
+      array[idx + 3] = 255;
+    }
+
+    result.push(array);
+  });
+
+  return result;
+};
