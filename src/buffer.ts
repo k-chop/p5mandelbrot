b58c783512f4120770b0b8b2f112e43761589c93
k-chop <whelmaze@gmail.com>
2022-11-26 13:03:41 +0900

    bufferを一部切り取ってコピーする関数の追加

--- src/buffer.ts
+++ src/buffer.ts
@@ -1,4 +1,4 @@
-export const copyBufferAsRect = (
+export const copyWholeBufferAsRect = (
   dest: Uint32Array,
   src: Uint32Array,
   destWidth: number,
@@ -13,5 +13,30 @@ export const copyBufferAsRect = (
     const srcWidth = endX - startX;
     const destOffset = (srcY + startY) * destWidth + startX;
     dest.set(src.subarray(srcWidth * srcY, srcWidth * (srcY + 1)), destOffset);
+  }
+};
+
+export const copyBufferRectToRect = (
+  dest: Uint32Array,
+  src: Uint32Array,
+  destWidth: number,
+  srcWidth: number,
+  clipWidth: number,
+  clipHeight: number,
+  destX: number,
+  destY: number,
+  srcX: number,
+  srcY: number
+) => {
+  const destHeight = Math.floor(dest.length / destWidth);
+
+  const actualHeight = Math.min(clipHeight, destHeight - destY);
+  const actualWidth = Math.min(clipWidth, destWidth - destX);
+
+  for (let y = 0; y < actualHeight; y++) {
+    const srcOffset = (y + srcY) * srcWidth + srcX;
+    const srcLine = src.subarray(srcOffset, srcOffset + actualWidth);
+    const destOffset = (y + destY) * destWidth + destX;
+    dest.set(srcLine, destOffset);
   }
 };
