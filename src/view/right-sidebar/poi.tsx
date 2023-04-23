c7c24ecbf9ef63fc0c79f7595606afb19b9340b6
k-chop <whelmaze@gmail.com>
2023-04-23 11:49:24 +0900

    スクロールバーがCardに重なるのが気になったので修正

--- src/view/right-sidebar/poi.tsx
+++ src/view/right-sidebar/poi.tsx
@@ -1,16 +1,27 @@
-import { ActionIcon, Button, Group, ScrollArea } from "@mantine/core";
+import {
+  ActionIcon,
+  Button,
+  Container,
+  Group,
+  ScrollArea,
+  Stack,
+} from "@mantine/core";
 import { POICard } from "./poi-card";
 import { IconCirclePlus } from "@tabler/icons-react";
 
 export const POI = () => {
   return (
-    <ScrollArea.Autosize mah={500}>
-      <Group position="right" mb="xs">
-        <Button variant="default" leftIcon={<IconCirclePlus />}>
-          Save POI
-        </Button>
-      </Group>
-      <POICard />
-    </ScrollArea.Autosize>
+    <ScrollArea h={500} offsetScrollbars>
+      <Container pl="0">
+        <Group position="right" mb="xs">
+          <Button variant="default" leftIcon={<IconCirclePlus />}>
+            Save POI
+          </Button>
+        </Group>
+        <Stack>
+          <POICard />
+        </Stack>
+      </Container>
+    </ScrollArea>
   );
 };
