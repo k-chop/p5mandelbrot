c5082a19852b7c28aa5a9eaea675619e69cca54f
k-chop <whelmaze@gmail.com>
2023-08-06 13:03:02 +0900

    ScrollAreaを置き換え

--- src/view/right-sidebar/poi.tsx
+++ src/view/right-sidebar/poi.tsx
@@ -1,16 +1,16 @@
-import { Container, Group, ScrollArea, Stack } from "@mantine/core";
 import { POICard } from "./poi-card";
 import { IconCirclePlus } from "@tabler/icons-react";
 import { usePOI } from "./use-poi";
 import { cloneParams, getCurrentParams } from "../../mandelbrot";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export const POI = () => {
   const { poiList, addPOI, deletePOIAt, applyPOI } = usePOI();
 
   return (
-    <ScrollArea h={500} offsetScrollbars>
-      <div>
+    <>
+      <div className="pr-4">
         <div className="mb-2 flex justify-end">
           <Button
             variant="default"
@@ -20,17 +20,21 @@ export const POI = () => {
             Save POI
           </Button>
         </div>
-        <Stack>
-          {poiList.map((poi, index) => (
-            <POICard
-              key={index}
-              poi={poi}
-              onDelete={() => deletePOIAt(index)}
-              onApply={() => applyPOI(poi)}
-            />
-          ))}
-        </Stack>
       </div>
-    </ScrollArea>
+      <ScrollArea className="h-[500px]">
+        <div className="pr-4">
+          <div className="flex flex-col gap-2">
+            {poiList.map((poi, index) => (
+              <POICard
+                key={index}
+                poi={poi}
+                onDelete={() => deletePOIAt(index)}
+                onApply={() => applyPOI(poi)}
+              />
+            ))}
+          </div>
+        </div>
+      </ScrollArea>
+    </>
   );
 };
