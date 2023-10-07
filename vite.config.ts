95bc59bc3e535cd0f25ca3cbc5ee88eec4dd7bfc
k-chop <whelmaze@gmail.com>
2023-10-07 10:02:59 +0900

    Add build setting for Cloudflare pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/p5mandelbrot/",
+  base: process.env.CLOUDFLARE_BUILD === "true" ? "/" : "/p5mandelbrot/",
   plugins: [react()],
   resolve: {
     alias: {
