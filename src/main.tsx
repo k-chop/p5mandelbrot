ddc2ae1ec095c0ddf69921a173f26383a962e48c
k-chop <whelmaze@gmail.com>
2023-10-07 11:59:55 +0900

    POIList引き継げないので問答無用リダイレクトはやめる

--- src/main.tsx
+++ src/main.tsx
@@ -306,11 +306,6 @@ const sketch = (p: p5) => {
 };
 
 const entrypoint = () => {
-  if (location.origin === "https://k-chop.github.io") {
-    location.assign("https://p5mandelbrot.pages.dev");
-    return;
-  }
-
   resetWorkers();
 
   createStore({
