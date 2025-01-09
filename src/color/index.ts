77d54e385c89703f3180717a533a8ca1d36a77be
k-chop <whelmaze@gmail.com>
2025-01-09 16:20:33 +0900

    Share URL時にPaletteを復元できるようにした

--- src/color/index.ts
+++ src/color/index.ts
@@ -3,3 +3,5 @@ export type { Palette } from "./model";
 export { chromaJsPalettes } from "./color-chromajs";
 export { d3ChromaticPalettes } from "./color-d3-chromatic";
 export { othersPalettes } from "./color-others";
+
+export { deserializePalette } from "./deserializer";
