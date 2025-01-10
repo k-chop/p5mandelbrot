a0c881bb19550cf38e03bfe613c786072541ec85
k-chop <whelmaze@gmail.com>
2025-01-10 15:34:52 +0900

    とりあえず左上に表示しておく

---
+++ src/components/loading-spinner.tsx
@@ -0,0 +1,5 @@
+import { LoaderCircle } from "lucide-react";
+
+export const LoadingSpinner = () => {
+  return <LoaderCircle className="size-16 animate-spin" />;
+};
---
+++ src/view/canvas-overlay/index.tsx
@@ -0,0 +1,9 @@
+import { LoadingSpinner } from "@/components/loading-spinner";
+
+export const CanvasOverlay = () => {
+  return (
+    <div className="size-[800px]">
+      <LoadingSpinner />
+    </div>
+  );
+};
