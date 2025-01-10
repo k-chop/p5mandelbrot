db37d7808fb1022207e4a57215a35a3da78a52d4
k-chop <whelmaze@gmail.com>
2025-01-10 13:55:28 +0900

    eslint-plugin-react-hooksいれた

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,22 @@
 // @ts-check
 
 import eslint from "@eslint/js";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 
+import { fixupPluginRules } from "@eslint/compat";
+
 export default tseslint.config(
+  {
+    plugins: {
+      // @ts-expect-error 型が合わない
+      "react-hooks": fixupPluginRules(pluginReactHooks),
+    },
+    // @ts-expect-error 型が合わない
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+    },
+  },
   { ignores: ["dist/", "src/components/ui/"] },
   eslint.configs.recommended,
   tseslint.configs.recommended,
