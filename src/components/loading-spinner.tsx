c21bc40b5fb58f45867bdaa275471ec9323ba28c
k-chop <whelmaze@gmail.com>
2025-01-10 15:45:21 +0900

    loading時だけ表示する

--- src/components/loading-spinner.tsx
+++ src/components/loading-spinner.tsx
@@ -1,5 +1,7 @@
 import { LoaderCircle } from "lucide-react";
 
 export const LoadingSpinner = () => {
-  return <LoaderCircle className="size-16 animate-spin" />;
+  return (
+    <LoaderCircle className="size-8 animate-spin text-gray-100 drop-shadow-[0_0_3px_rgba(0,0,0,1)]" />
+  );
 };
