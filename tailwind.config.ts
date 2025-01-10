12ac3adc3e6e9ee2c9ee1c5802edd7deda88d95e
k-chop <whelmaze@gmail.com>
2025-01-10 13:03:22 +0900

    tailwindconfigをtsに

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+import animatePlugin from "tailwindcss-animate";
+import windyRadixPalettePlugin from "windy-radix-palette";
+
+export default {
   darkMode: "class",
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -59,12 +62,12 @@ module.exports = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
@@ -73,5 +76,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("windy-radix-palette")],
-};
+  plugins: [animatePlugin, windyRadixPalettePlugin],
+} satisfies Config;
