5ff5f3633fcc8121bdaf3bb63550bdaa8f29ed7f
k-chop <whelmaze@gmail.com>
2025-01-13 16:13:08 +0900

    なんか毎回ロードしているのでReact.memo

--- src/view/right-sidebar/poi-card-preview.tsx
+++ src/view/right-sidebar/poi-card-preview.tsx
@@ -1,12 +1,13 @@
 import { loadPreview, useTrackChangePreview } from "@/store/preview-store";
 import { POIData } from "@/types";
+import React from "react";
 import usePromise from "react-promise-suspense";
 
 type Props = {
   poi: POIData;
 };
 
-export const POICardPreview = ({ poi }: Props) => {
+export const POICardPreview = React.memo(({ poi }: Props) => {
   const data = usePromise(loadPreview, [poi.id], 1000);
 
   useTrackChangePreview(poi.id);
@@ -19,4 +20,5 @@ export const POICardPreview = ({ poi }: Props) => {
     );
 
   return <img src={data} />;
-};
+});
+POICardPreview.displayName = "POICardPreview";
